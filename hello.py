d20cae030117057a144311a15e640bcd2fd2691f
MJ <micaeljin01@gmail.com>
2024-03-18 22:35:15 +0000

    write add function

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
 print("hello world")
 print("fix erro")
-=======
+
 print("hello world!!!!!!!!!!!!!!!!!!!!!!!!!")
 print("asdasd a")
 #asdad
@@ -18,4 +18,17 @@ print("modify hello-test")
 
 
 print("change hello.py file")
->>>>>>> hello-test
+# >>>>>>> hello-test
+
+
+
+
+
+
+
+def add(a, b):
+    return a + b
+
+x = 1
+y = 9
+print(add(x, y))
